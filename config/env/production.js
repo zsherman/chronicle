bbfcf5b09e140ea95d31b91623e2b98498d7a151
Zach Sherman <zksherm@gmail.com>
2015-04-08 22:30:44 -0400

    use mongohq

--- config/env/production.js
+++ config/env/production.js
@@ -4,8 +4,7 @@
  */
 
 module.exports = {
-  // db: process.env.MONGOHQ_URL,
-  db: process.env.MONGOLAB_URI,
+  db: process.env.MONGOHQ_URL,
   facebook: {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
